6a2cc649987afdb374130663aba70bb2e110925b
Pierce Gearhart <61991125+piercegearhart@users.noreply.github.com>
2023-12-15 20:03:10 -0500

    changes

--- src/components/sections/title.tsx
+++ src/components/sections/title.tsx
@@ -4,7 +4,7 @@ type props = {
 
 const Title: React.FC<props> = ({ size, children }) => {
     return (
-        <h1 className={'font-medium mb-4' + (size === 'sm' ? 'dark:text-zinc-200 text-2xl' : 'dark:text-zinc-100 text-3xl')}>
+        <h1 className={'font-medium mb-4 ' + (size === 'sm' ? 'dark:text-zinc-200 text-2xl' : 'dark:text-zinc-100 text-3xl')}>
             {children}
         </h1>
     )
